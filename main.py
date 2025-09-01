7b8ea7d47d783ee1166eaa431622664485f19c4b
Durashca <wladkosmylev@yandex.ru>
2025-09-01 23:15:39 +0300

    change text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Label(text="Привет, мир!")
+        return Label(text="Привет, мир!!")
 
 if __name__ == '__main__':
     MyApp().run()
